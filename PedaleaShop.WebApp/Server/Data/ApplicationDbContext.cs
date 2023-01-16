81fc3a7f3aacee9c201a719ea6a4e8b3a6d83fca
David Imbajoa <deivy311@hotmail.com>
2023-01-16 01:32:25 -0500

    Base Login created

---
+++ PedaleaShop.WebApp/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PedaleaShop.WebApp.Server.Models;
+
+namespace PedaleaShop.WebApp.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
